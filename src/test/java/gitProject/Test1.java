82d747e99611d1d0d106fc077720b9511e80bb5d
Volkan K <vkaraca79@hotmail.com>
2021-09-11 22:09:37 +0300

    My second commit

---
+++ src/test/java/gitProject/Test1.java
@@ -0,0 +1,9 @@
+package gitProject;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("My first project with GIT");
+
+        System.out.println("I made some more changes");
+    }
+}
